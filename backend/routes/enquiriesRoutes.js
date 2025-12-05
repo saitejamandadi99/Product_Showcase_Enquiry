2d172f10e181fdfd3d849566efb20af55b9129af
saitejamandadi99 <saitejamandadi99>
2025-12-05 16:04:50 +0530

    created a middleware and added it to routes for protected information

--- backend/routes/enquiriesRoutes.js
+++ backend/routes/enquiriesRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middleware/authMiddleware')
 const {createEnquiry, getAllEnquiries} = require('../controllers/enquiriesController')
 
+
 router.post('/', createEnquiry)
-router.get('/', getAllEnquiries)
+router.get('/',authMiddleware, getAllEnquiries)
 
 module.exports = router
