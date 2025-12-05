b2a1edba88cf8ac30ea9172eb4c4122e985eed92
saitejamandadi99 <saitejamandadi99>
2025-12-05 11:21:14 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.send('Application is running in the backend')
+})
+
+app.use((err, req , res, next)=>{
+    console.error(err.message)
+    res.status(500).json({message: err.message || 'Internal server error'})
+}) //global error middleware
+
+const PORT = process.env.PORT || 5000 
+app.listen(PORT,()=>{
+    console.log(`application is running in http://localhost:${PORT}`)
+})
