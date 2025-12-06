2abf9face86326dbb9524a145b3ad91f6400535d
saitejamandadi99 <saitejamandadi99>
2025-12-06 17:23:47 +0530

    added the header and tested it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
 import MainPage from "./pages/mainPage/MainPage.jsx";
 import ProductDetails from "./pages/ProductDetails/ProductDetails.jsx";
 import Login from "./pages/Login/Login.jsx";
@@ -10,6 +11,7 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/product/:id" element={<ProductDetails />} />
