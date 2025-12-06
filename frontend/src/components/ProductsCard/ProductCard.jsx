ef0618dff72203085cbcae7d163474142226285a
saitejamandadi99 <saitejamandadi99>
2025-12-06 11:40:15 +0530

    completed the product card details

--- frontend/src/components/ProductsCard/ProductCard.jsx
+++ frontend/src/components/ProductsCard/ProductCard.jsx
@@ -1,15 +1,20 @@
-
+import './productCard.css'
 const ProductCard= (props) =>{
-    const {productDetails} = props 
-    const {name, description , price , category, image} = productDetails
+    const {productDetails, onViewDetails} = props 
+    const {id,name, description , price , category, image} = productDetails
 
     return(
-        <li>
-            <h4>{name}</h4>
-            <h6>{description.slice(0, 40)}...</h6>
-            <h5>{price}</h5>
-            <h5>{category}</h5>
-            <img src={image} alt={name} />
+        <li className="product-card">
+            <img src={image} alt={name} className="product-image" />
+            <div className="product-content">
+                <h4 className="product-title">{name}</h4>
+                <h6 className="product-desc">{description.slice(0, 40)}...</h6>
+                <h5 className="product-category">{category}</h5>
+                <h5 className="product-price">{price}Rs/-</h5>
+                <button type='button' className='details-btn' onClick={()=>onViewDetails(id)}>
+                    View Details
+                </button>
+            </div>
         </li>
     )
 
