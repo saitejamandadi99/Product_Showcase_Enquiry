2bea448594cabfa6c774453f56443b3b7cfb3661
saitejamandadi99 <saitejamandadi99>
2025-12-06 13:37:09 +0530

    completed the mainpage

--- frontend/src/pages/mainPage/MainPage.jsx
+++ frontend/src/pages/mainPage/MainPage.jsx
@@ -1,4 +1,6 @@
-import { useState, useEffect,useNavigate } from "react"
+
+import { useState, useEffect } from "react"
+import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
 import ProductCard from '../../components/ProductsCard/ProductCard.jsx'
 import './mainpage.css'
@@ -11,20 +13,24 @@ const MainPage = () =>{
     const [search,setSearch] = useState('')
     const [category, setCategory] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
     const navigate  = useNavigate()
     
     const handleViewDetails = (id)=>{
-        navigate(`/products/:${id}`)
+        navigate(`/product/${id}`)
     }
 
     const getProductsList = async ()=>{
         setIsLoading(true)
         try {
 
-            const url = `${process.env.REACT_APP_API_URL}/api/products?search=${search}&category=${category}&page=1&limit=10`
+            const url = `${process.env.REACT_APP_API_URL}/api/products?search=${search}&category=${category}&page=${page}&limit=10`
             const response = await axios.get(url)
             setSuccess("Products fetched successfully")
             setProductList(response.data.products)
+            setTotalPages(response.data.pagination.totalPages);
             setError('')
             
         } catch (error) {
@@ -38,7 +44,7 @@ const MainPage = () =>{
 
     useEffect(()=>{
         getProductsList()
-    },[search,category]) 
+    },[search,category,page]) 
 
     const renderProducts = () =>{
         return(
@@ -47,10 +53,68 @@ const MainPage = () =>{
                <p>No products found</p> 
             ):(
                 <>
-                <div className="filter-container">
-                    <label htmlFor="searchInput">Search</label>
-                     <input value = {search} type='search' id='searchInput' onChange={e=>setSearch(e.target.value)} />
-                     <select value={category} onChange={e=>setCategory(e.target.value)}>
+                <ul >
+                    {
+                        productList.map(eachProduct=>(
+                            <ProductCard key={eachProduct.id} productDetails={eachProduct} onViewDetails={handleViewDetails} />
+                        ))
+                    }    
+
+                </ul>
+                <div className="pagination-container">
+                    <button 
+                        disabled={page === 1}
+                        onClick={() => setPage(page - 1)}
+                    >
+                        Prev
+                    </button>
+
+                    <span>Page {page} of {totalPages}</span>
+
+                    <button 
+                        disabled={page === totalPages}
+                        onClick={() => setPage(page + 1)}
+                    >
+                        Next
+                    </button>
+                    </div>
+
+                </>
+            )}
+
+            {success && (
+            <p style={{ backgroundColor: "green", color: "white", padding: "8px", textAlign:'center' }}>
+                {success}
+            </p>
+            )}
+
+            {error && (
+            <p style={{ backgroundColor: "red", color: "white", padding: "8px" , textAlign:'center' }}>
+            {error}
+            </p>
+            )}
+
+            
+            </>
+        )
+    }
+    return(
+        <div className="mainContainer">
+            <div className="filter-container">
+                    <div className="filter-group">
+                        <label htmlFor="searchInput">Search</label>
+                        <input value = {search} type='search' id='searchInput' onChange={(e) => {
+                                                                    setSearch(e.target.value);
+                                                                    setPage(1); 
+                                                                    }}/>
+                    </div>
+                    <div className="filter-group">
+                        <label>Category</label>
+                    
+                     <select value={category} onChange={(e) => {
+                                            setCategory(e.target.value);
+                                            setPage(1);
+                                        }}>
                         <option value=''>All</option>
                         <option value='Electronics'>Electronics</option>
                         <option value='Fashion'>Fashion</option>
@@ -60,27 +124,9 @@ const MainPage = () =>{
                         <option value='Fitness'>Fitness</option>
                         <option value='Books'>Books</option>
                      </select>
-
+                    </div>                           
                 </div>
-               
-                <ul>
-                    {
-                        productList.map(eachProduct=>(
-                            <ProductCard key={eachProduct.id} productDetails={eachProduct} onViewDetails={handleViewDetails} />
-                        ))
-                    }    
 
-                </ul>
-                </>
-            )}
-            {success && <p>{success}</p>}
-            {error && <p>{error}</p>}
-            
-            </>
-        )
-    }
-    return(
-        <div className="mainContainer">
             {isLoading? (
                 <h3>Loading...</h3>
             ):(
